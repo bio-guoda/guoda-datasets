eec585b110862bf187e61ae540655d26a5a71e00
Matthew J. Collins <mcollins@acis.ufl.edu>
2016-06-02 21:35:44 -0400

    Full run works - have to allocate enough RAM for whole 64 GB dataset to write out

--- BHL/nb.sh
+++ BHL/nb.sh
@@ -3,5 +3,5 @@
 # http://ramhiser.com/2015/02/01/configuring-ipython-notebook-support-for-pyspark/
 export SPARK_HOME="/opt/spark"
 #export PYSPARK_SUBMIT_ARGS="--master cloudera0.acis.ufl.edu"
-export PYSPARK_SUBMIT_ARGS="--executor-memory 10G --driver-memory 10G --num-executors 3 --executor-cores 5 --jars ${SPARK_HOME}/jars/elasticsearch-hadoop-2.2.0.jar pyspark-shell "
+export PYSPARK_SUBMIT_ARGS="--executor-memory 80G --driver-memory 80G --num-executors 3 --executor-cores 5 --jars ${SPARK_HOME}/jars/elasticsearch-hadoop-2.2.0.jar pyspark-shell "
 ipython notebook --profile=pyspark --ip='*' --no-browser
