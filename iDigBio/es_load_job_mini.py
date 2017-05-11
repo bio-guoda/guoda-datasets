0f813e070ba299e47e39325c55ad346b8e0f9799
Matthew Collins <mcollins@acis.ufl.edu>
2017-05-11 08:06:47 -0400

    Added a small testing job to exercise the toolchain

---
+++ iDigBio/es_load_job_mini.py
@@ -0,0 +1,57 @@
+# A quick test job to exercise the whole stack w/o rewriting this to be more
+# general.
+
+from __future__ import print_function
+import os
+import sys
+import time
+import requests
+from pyspark import SparkContext, SQLContext
+import pyspark.sql.functions as sql
+import pyspark.sql.types as types
+
+
+sc = SparkContext(appName="iDigBioParquetMini")
+sqlContext = SQLContext(sc)
+
+dataset_date = time.strftime("%Y%m%d%s")
+out_dir = "/outputs/mini-idigbio-{0}.parquet".format(dataset_date)
+
+# This doesn't work for checking HDFS file system, will always overwrite output
+if os.path.isdir(out_dir):
+    print("Output dir {0} exists, use --overwrite?".format(out_dir))
+    exit
+
+index = "idigbio"
+nodes = "c18node14.acis.ufl.edu,c18node2.acis.ufl.edu,c18node6.acis.ufl.edu,c18node10.acis.ufl.edu,c18node12.acis.ufl.edu"
+query = '{"query": {"bool": {"must": [{"term":{"genus":"acer"}}]}}}'
+
+# Get field list from API endpoint
+meta_fields_records = (requests
+                       .get("http://search.idigbio.org/v2/meta/fields/records")
+                       .json()
+                       )
+field_set = set()
+for k,v in meta_fields_records.items():
+    if v.get("fieldName", False):
+        field_set.add(k)
+    if k == "data":
+        for kd,vd in v.items():
+            if vd.get("fieldName", False):
+                field_set.add("data.{0}".format(kd))
+
+# Remove known fields that cause problems
+bad_field_set = set({'commonnames', 'flags', 'recordids', 'mediarecords'})
+field_set -= bad_field_set
+fields = ",".join(field_set)
+
+# Write out dataframe
+(sqlContext.read.format("org.elasticsearch.spark.sql")
+    .option("es.read.field.include", fields)
+    .option("es.nodes", nodes)
+    .option("es.query", query)
+    .load("{0}/records".format(index))
+    .write
+    .mode("overwrite")
+    .parquet(out_dir)
+)
---
+++ iDigBio/es_load_job_mini_run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+spark-submit --driver-memory 50g es_load_job_mini.py
