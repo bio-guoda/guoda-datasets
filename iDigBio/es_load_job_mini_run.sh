bb0d7e1acefaa393f2f7b262062641437b7d5a5e
Matthew Collins <mcollins@acis.ufl.edu>
2017-05-12 16:14:07 -0400

    Adjusted resources to job will run well on cluster

--- iDigBio/es_load_job_mini_run.sh
+++ iDigBio/es_load_job_mini_run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-spark-submit --master mesos://mesos01.acis.ufl.edu:5050 --driver-memory 50g es_load_job_mini.py
+spark-submit --master mesos://mesos01.acis.ufl.edu:5050 \
+             --driver-memory 2g \
+             --total-executor-cores 48 \
+             --executor-memory 1250M \
+             es_load_job_mini.py
