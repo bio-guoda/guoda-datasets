65aec716fd30b398e5756850bcbd0d4e9ce2d456
Matthew Collins <mcollins@acis.ufl.edu>
2017-05-18 15:02:24 -0400

    Submittable job but not debugged.

--- iDigBio/es_load_job_mini_run.sh
+++ iDigBio/es_load_job_mini_run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 /opt/spark/latest/bin/spark-submit \
-             --master mesos://mesos01.acis.ufl.edu:5050 \
+             --master mesos://mesos03.acis.ufl.edu:5050 \
              --driver-memory 2g \
              --total-executor-cores 48 \
              --executor-memory 1250M \
+             --proxy-user hdfs \
              es_load_job_mini.py
