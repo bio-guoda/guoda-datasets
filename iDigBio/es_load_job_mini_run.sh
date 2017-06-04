85ee865702415d899a5138bf44ff03b58a64d0ec
Matthew Collins <mcollins@acis.ufl.edu>
2017-06-03 22:10:40 -0400

    Full path for executable, Jenkins user missing .profile

--- iDigBio/es_load_job_mini_run.sh
+++ iDigBio/es_load_job_mini_run.sh
@@ -3,7 +3,8 @@
 export HADOOP_CONF_DIR="/etc/hadoop/conf"
 export HADOOP_USER_NAME="hdfs"
 
-spark-submit --master mesos://zk://mesos02:2181,mesos01:2181,mesos03:2181/mesos \
+/opt/spark/latest/spark-submit \
+             --master mesos://zk://mesos02:2181,mesos01:2181,mesos03:2181/mesos \
              --driver-memory 2g \
              --total-executor-cores 48 \
              --executor-memory 1250M \
