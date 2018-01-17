8b37915dcfbe98f51b02bc491bc917e34f5a7f74
Matthew J Collins <mjcollin@users.noreply.github.com>
2018-01-16 20:24:54 -0500

    Increase memory for executors
    Memory for executors should be the memory on the node, not cores*x = memory on nodes

--- iDigBio/es_load_job_run.sh
+++ iDigBio/es_load_job_run.sh
@@ -7,6 +7,6 @@ export HADOOP_USER_NAME="hdfs"
              --master mesos://zk://mesos02:2181,mesos01:2181,mesos03:2181/mesos \
              --driver-memory 2G \
              --total-executor-cores 48 \
-             --executor-memory 3G \
+             --executor-memory 20G \
              --conf spark.sql.shuffle.partitions=3000 \
              es_load_job.py
