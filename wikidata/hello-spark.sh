c7b7407583322e03b22883addff92a0a0c631426
jhpoelen <jhpoelen@xs4all.nl>
2018-04-04 19:23:18 -0700

    more testing

--- wikidata/hello-spark.sh
+++ wikidata/hello-spark.sh
@@ -4,5 +4,5 @@
 #
 
 #source /etc/hadoop/conf/hadoop-env.sh
-/opt/spark/latest/bin/spark-shell --master mesos://zk://mesos01:2182,mesos02:2181,mesos03:2181/mesos --driver-memory 4G --executor-memory 4G < println ("hello world")
+/opt/spark/latest/bin/spark-shell --master mesos://zk://mesos01:2182,mesos02:2181,mesos03:2181/mesos --driver-memory 4G --executor-memory 4G < echo "println (\"hello world\")"
 
